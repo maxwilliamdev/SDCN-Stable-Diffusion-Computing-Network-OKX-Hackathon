65a4ef65784b8a91c6ef1308850fea9e870ba2f8
troy6en <troy6en@gmail.com>
2023-03-26 06:19:03 +0800

    feat(sdcnToken): impl transferTokens

---
+++ sdcn-web/src/api/sdcnToken/index.tsx
@@ -0,0 +1,34 @@
+import { BigNumberish, ethers } from 'ethers'
+import { wallet } from 'wallets/wallet'
+import { Erc20Abi__factory } from 'typings/Erc20Abi'
+import { TransactionReceipt } from '@ethersproject/providers'
+
+const contractAddress = '0xf93ddfc544927fd55a1bea06c412723266d232c0'
+
+export async function transferTokens(
+  to: string,
+  amount: BigNumberish,
+): Promise<TransactionReceipt> {
+  const signer = await wallet.getSigner()
+  const provider = wallet.getProvider()
+
+  const contractInstance = Erc20Abi__factory.connect(contractAddress, signer)
+
+  const options = {
+    gasLimit: '200000',
+    gasPrice: ethers.utils.parseUnits('10', 'gwei'),
+  }
+  await contractInstance.connect(signer)
+  const tx = await contractInstance.transfer(to, amount, options)
+  console.log('SDCN token Transaction sent:', tx.hash)
+
+  await tx.wait()
+
+  const receipt = await provider.getTransactionReceipt(tx.hash)
+  if (receipt.status === 1) {
+    console.log('Transaction succeeded!')
+  } else {
+    console.error('Transaction failed:', receipt)
+  }
+  return receipt
+}
